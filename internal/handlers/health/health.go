b491eab6593405067d388b61393b4e1c5ba84fae
Austin Sofaer <164747892+V4N1LLA-1CE@users.noreply.github.com>
2024-12-30 16:25:02 +1100

    feat: implement health handler

---
+++ internal/handlers/health/health.go
@@ -0,0 +1,30 @@
+package health
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/V4N1LLA-1CE/netio"
+)
+
+type Handler struct {
+	env string
+}
+
+func New(env string) *Handler {
+	return &Handler{
+		env: env,
+	}
+}
+
+func (h *Handler) Check(w http.ResponseWriter, r *http.Request) {
+	health := map[string]any{
+		"status":      "alive",
+		"environment": h.env,
+		"timestamp":   time.Now(),
+	}
+
+	if err := netio.Write(w, http.StatusOK, netio.Envelope{"health": health}, nil); err != nil {
+		http.Error(w, err.Error(), http.StatusOK)
+	}
+}
