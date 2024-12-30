d00a27c49c61cc554e1075613386940725f08141
Austin Sofaer <164747892+V4N1LLA-1CE@users.noreply.github.com>
2024-12-30 21:44:03 +1100

    fix: bcrypt hash password before storing in db

--- internal/handlers/auth/registration.go
+++ internal/handlers/auth/registration.go
@@ -8,6 +8,7 @@ import (
 	"github.com/V4N1LLA-1CE/netio"
 	"github.com/V4N1LLA-1CE/social-app/internal/store"
 	"github.com/V4N1LLA-1CE/social-app/internal/validators"
+	"golang.org/x/crypto/bcrypt"
 )
 
 func (h *Handler) Register(w http.ResponseWriter, r *http.Request) {
@@ -39,11 +40,18 @@ func (h *Handler) Register(w http.ResponseWriter, r *http.Request) {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
+	// hash password with bcrypt
+	passwordHash, err := bcrypt.GenerateFromPassword([]byte(input.PasswordPlaintext), 12)
+	if err != nil {
+		netio.Error(w, v.Errors, http.StatusInternalServerError)
+		return
+	}
+
 	// create user
 	user := &store.User{
 		Username: input.Username,
 		Email:    input.Email,
-		Password: input.PasswordPlaintext,
+		Password: passwordHash,
 	}
 
 	if err := h.store.Users.Create(ctx, user); err != nil {
--- internal/store/users.go
+++ internal/store/users.go
@@ -10,7 +10,7 @@ type User struct {
 	ID        int64     `json:"id"`
 	Username  string    `json:"username"`
 	Email     string    `json:"email"`
-	Password  string    `json:"-"`
+	Password  []byte    `json:"-"`
 	CreatedAt time.Time `json:"created_at"`
 }
 
