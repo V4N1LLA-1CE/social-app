c4e5a9e5d4f1659867b65acf8c36bed311fb6116
Austin Sofaer <164747892+V4N1LLA-1CE@users.noreply.github.com>
2024-12-30 21:32:08 +1100

    feat: cors middleware and new register endpoint

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -1,19 +1,35 @@
 package main
 
 import (
+	"github.com/V4N1LLA-1CE/social-app/internal/handlers/auth"
 	"github.com/V4N1LLA-1CE/social-app/internal/handlers/health"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
 )
 
 func (app *application) mount() *chi.Mux {
-	// initialise all handlers
+	// initialise handlers
 	healthHandler := health.New(app.config.env)
+	authHandler := auth.New(app.store)
 
 	// init router
 	r := chi.NewRouter()
 
-	// global middleware
+	// global middlewares
+	//
+	// Basic CORS
+	// for more ideas, see: https://developer.github.com/v3/#cross-origin-resource-sharing
+	r.Use(cors.Handler(cors.Options{
+		// AllowedOrigins:   []string{"https://foo.com"}, // Use this to allow specific origin hosts
+		AllowedOrigins: []string{"https://*", "http://*"},
+		// AllowOriginFunc:  func(r *http.Request, origin string) bool { return true },
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: true,
+		MaxAge:           300, // Maximum value not ignored by any of major browsers
+	}))
 	r.Use(middleware.RequestID)
 	r.Use(middleware.RealIP)
 	r.Use(middleware.Logger)
@@ -22,6 +38,10 @@ func (app *application) mount() *chi.Mux {
 	// routes
 	r.Route("/api/v1", func(r chi.Router) {
 		r.Get("/health", healthHandler.Check)
+
+		r.Route("/auth", func(r chi.Router) {
+			r.Post("/register", authHandler.Register)
+		})
 	})
 
 	return r
