49fd8ce9c068b6c04c59e653b798b0f34671a0e0
Austin Sofaer <164747892+V4N1LLA-1CE@users.noreply.github.com>
2024-12-31 17:29:49 +1100

    refactor: api errors and password setting

--- internal/handlers/auth/registration.go
+++ internal/handlers/auth/registration.go
@@ -8,60 +8,52 @@ import (
 	"github.com/V4N1LLA-1CE/netio"
 	"github.com/V4N1LLA-1CE/social-app/internal/store"
 	"github.com/V4N1LLA-1CE/social-app/internal/validators"
-	"golang.org/x/crypto/bcrypt"
 )
 
 func (h *Handler) Register(w http.ResponseWriter, r *http.Request) {
 	var input struct {
-		Username          string `json:"username"`
 		Email             string `json:"email"`
 		PasswordPlaintext string `json:"password"`
 	}
 
+	// read json body into input struct
 	err := netio.Read(w, r, &input)
 	if err != nil {
-		netio.Error(w, err.Error(), http.StatusUnprocessableEntity)
+		netio.Error(w, "error", err.Error(), http.StatusUnprocessableEntity)
 		return
 	}
 
 	// validate input
 	v := netio.NewValidator()
 
-	validators.ValidateUsername(v, input.Username)
 	validators.ValidateEmail(v, input.Email)
 	validators.ValidatePassword(v, input.PasswordPlaintext)
 
 	if !v.Valid() {
-		netio.Error(w, v.Errors, http.StatusUnprocessableEntity)
+		netio.Error(w, "error", v.Errors, http.StatusUnprocessableEntity)
 		return
 	}
 
-	// 5 second timeout on user creation
+	// create user with default values
+	// set email and attempt to set password
+	user := &store.User{
+		Email: input.Email,
+	}
+	if err := user.Password.Set(input.PasswordPlaintext); err != nil {
+		netio.Error(w, "error", err.Error(), http.StatusInternalServerError)
+	}
+
+	// create user in db with 5 second timeout
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
-
-	// hash password with bcrypt
-	passwordHash, err := bcrypt.GenerateFromPassword([]byte(input.PasswordPlaintext), 12)
-	if err != nil {
-		netio.Error(w, v.Errors, http.StatusInternalServerError)
-		return
-	}
-
-	// create user
-	user := &store.User{
-		Username: input.Username,
-		Email:    input.Email,
-		Password: passwordHash,
-	}
-
 	if err := h.store.Users.Create(ctx, user); err != nil {
-		netio.Error(w, err.Error(), http.StatusInternalServerError)
+		netio.Error(w, "error", err.Error(), http.StatusInternalServerError)
 		return
 	}
 
 	// send response
 	if err := netio.Write(w, http.StatusCreated, netio.Envelope{"user": user}, nil); err != nil {
-		netio.Error(w, err.Error(), http.StatusInternalServerError)
+		netio.Error(w, "error", err.Error(), http.StatusInternalServerError)
 		return
 	}
 }
--- internal/store/users.go
+++ internal/store/users.go
@@ -3,15 +3,36 @@ package store
 import (
 	"context"
 	"database/sql"
+	"strings"
 	"time"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
 type User struct {
 	ID        int64     `json:"id"`
-	Username  string    `json:"username"`
 	Email     string    `json:"email"`
-	Password  []byte    `json:"-"`
+	Password  Password  `json:"-"`
+	Activated bool      `json:"activation_status"`
 	CreatedAt time.Time `json:"created_at"`
+}
+
+type Password struct {
+	PasswordHash      []byte
+	PasswordPlaintext string
+}
+
+func (p *Password) Set(plaintext string) error {
+	// hash password with bcrypt
+	h, err := bcrypt.GenerateFromPassword([]byte(plaintext), 12)
+	if err != nil {
+		return err
+	}
+
+	p.PasswordPlaintext = plaintext
+	p.PasswordHash = h
+
+	return nil
 }
 
 type UserStore struct {
@@ -20,15 +41,15 @@ type UserStore struct {
 
 func (s *UserStore) Create(ctx context.Context, user *User) error {
 	query := `
-  INSERT INTO users (username, email, password) 
+  INSERT INTO users (email, password, activated) 
   VALUES ($1, $2, $3)
   RETURNING id, created_at
   `
 
 	args := []any{
-		user.Username,
 		user.Email,
-		user.Password,
+		user.Password.PasswordHash,
+		false,
 	}
 
 	err := s.db.QueryRowContext(ctx, query, args...).Scan(
@@ -37,7 +58,12 @@ func (s *UserStore) Create(ctx context.Context, user *User) error {
 	)
 
 	if err != nil {
-		return err
+		switch {
+		case strings.Contains(err.Error(), "users_email_key"):
+			return ErrDuplicateEmail
+		default:
+			return err
+		}
 	}
 
 	return nil
