78a1fafe8a463c14ab60dd96620209cadca104e0
Austin Sofaer <164747892+V4N1LLA-1CE@users.noreply.github.com>
2025-01-06 23:21:02 +1100

    fix: post model

--- internal/store/posts.go
+++ internal/store/posts.go
@@ -9,8 +9,8 @@ import (
 
 type Post struct {
 	ID        int64     `json:"id"`
-	Content   string    `json:"content"`
 	Title     string    `json:"title"`
+	Content   string    `json:"content"`
 	UserID    int64     `json:"user_id"`
 	Tags      []string  `json:"tags"`
 	CreatedAt time.Time `json:"created_at"`
